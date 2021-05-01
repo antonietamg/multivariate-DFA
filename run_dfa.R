41724f6105331f13c3dae1528830587408d92641
Antonieta Martínez-Guerrero <62116253+antonietamg@users.noreply.github.com>
2021-05-01 08:33:00 -0500

    Update run_dfa.R

--- run_dfa.R
+++ run_dfa.R
@@ -2,6 +2,6 @@
 
 #define data and run function
 
-x <- yourdata
-dplot <- dfa_function(x)
+x <- your_data
+dplot <- dfa_function(x, pol, sec) #x = dataframe; pol = polynomial; sec = scale values (for scale use get_scale function)
 write(alpha_val, file = "alphadfa")
